fd95ba090387c58a896b4ec0b850576f1182a04b
Alyssa Tonolete <alyssatonolete@gmail.com>
2025-08-11 11:28:42 +0100

    updated css

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ const deleteBtn = document.getElementById("delete-btn");
 const saveBtn = document.getElementById("save-btn");
 let recordEl = documentgetElementById("record-el");
 
-addBtn.addEventListener("click", function() {
+function test() {
+    console.log("hi");
+}
+
+addBtn.addEventListener("click", () => {
+    console.log("button clicked");
     count += 1;
     countEl.textContent = count;
 });
 
-deleteBtn.addEventListener("click", function() {
+deleteBtn.addEventListener("click", () => {
     count -= 1;
     countEl.textContent = count;
 });
 
-saveBtn.addEventListener("click", function() {
+saveBtn.addEventListener("click", () => {
     let recordString = `${count} - `;
     recordEl.textContent += recordString;
     countEl.textContent = 0;
